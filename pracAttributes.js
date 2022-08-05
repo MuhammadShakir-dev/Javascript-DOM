5b3ff165c699af4f2f41970a34f28e3ef6ce9c18
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-05 11:51:59 +0500

    Create pracAttributes.js

---
+++ pracAttributes.js
@@ -0,0 +1,12 @@
+// lets practice some DOM attributes in javascript.
+
+// Now there are two type of attribute methods in javascript .
+
+// getAttribute() & setAttribute();
+
+// getAttribute is used to get the attibute of any Html element.
+// setAttribute is used  to update the remove the attribute of an Html element.
+
+
+
+ 
