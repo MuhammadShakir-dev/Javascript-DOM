3e2abcbc48c639f33216d32a3c6657390ed9ad4a
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-05 15:33:24 +0500

    Update pracAttributes.js

--- pracAttributes.js
+++ pracAttributes.js
@@ -7,6 +7,16 @@
 // getAttribute is used to get the attibute of any Html element.
 // setAttribute is used  to update the remove the attribute of an Html element.
 
+// js
+
+const img = document.querySelectpr('img');
+img.getAttribute('src');
+img.setAttribute('src','https://images.unsplash.com/photo-1659653198554-a2bbfa8c6128?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60');
+
+// set attributes takes two values one is attribute name and the other its new value.
+
+
+
 
 
  
