c4e624f9756654e91c43514f7cb470f2831b6381
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-08-05 15:39:15 +0500

    Create stylesPrac.js

---
+++ stylesPrac.js
@@ -0,0 +1,11 @@
+// lets have a look on how to change styles of html elements using javascript.
+
+//js
+
+const h1 = doucment.querySelectr('h1');
+h1.style.fontWeigth = 'bold';
+
+const btn = document.querySelector('button');
+btn.style.fontSize = '25px';
+btn.style.fontFamily = 'Opensans';
+
